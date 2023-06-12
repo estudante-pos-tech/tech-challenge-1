bbb1c5b108ab44b5b59d7593e15f5a8c4d40451a
cesar(estudante) <estudante.pos.tech.java@gmail.com>
2023-06-12 15:28:29 -0300

    create AppConfiguration.java and add a default validator bean to spring context

---
+++ src/main/java/rm349040/techchallenge1/config/AppConfiguration.java
@@ -0,0 +1,16 @@
+package rm349040.techchallenge1.config;
+
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfiguration {
+
+    @Bean
+    public Validator beanValidator(){
+        return Validation.buildDefaultValidatorFactory().getValidator();
+    }
+
+}
