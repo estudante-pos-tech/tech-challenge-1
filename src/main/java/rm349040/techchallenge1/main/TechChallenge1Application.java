9632529f4d5b6263fd3c3033a6682a1ac6ac0579
cesar(estudante) <estudante.pos.tech.java@gmail.com>
2023-06-12 12:42:51 -0300

    Initial commit

---
+++ src/main/java/rm349040/techchallenge1/main/TechChallenge1Application.java
@@ -0,0 +1,13 @@
+package rm349040.techchallenge1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechChallenge1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechChallenge1Application.class, args);
+    }
+
+}
---
+++ src/test/java/rm349040/techchallenge1/TechChallenge1ApplicationTests.java
@@ -0,0 +1,13 @@
+package rm349040.techchallenge1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechChallenge1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
