5ba55a796dd29d18987a6f9a98d05aa4b5f8be19
cesar(estudante) <estudante.pos.tech.java@gmail.com>
2023-06-14 13:36:37 -0300

    create EletrodomesticoController e dados cadastro eletrodomestico DTO.

---
+++ src/main/java/rm349040/techchallenge1/apis/eletrodomesticos/controller/EletrodomesticoController.java
@@ -0,0 +1,19 @@
+package rm349040.techchallenge1.apis.eletrodomesticos.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import rm349040.techchallenge1.util.Validation;
+
+@RestController
+@RequestMapping("eletrodomestico")
+public class EletrodomesticoController {
+
+    @Autowired
+    private Validation validation;
+
+
+
+
+
+}
---
+++ src/main/java/rm349040/techchallenge1/apis/eletrodomesticos/controller/dtos/DadosCadastroEletrodomestico.java
@@ -0,0 +1,23 @@
+package rm349040.techchallenge1.apis.eletrodomesticos.controller.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record DadosCadastroEletrodomestico(
+
+
+        @NotBlank(message = "ERRO: o nome NÃO pode ser nulo ou em BRANCO")
+        @Size(max = 60, message = "ERRO: o nome do eletrodoméstico NÃO pode conter mais do que 60 chars")
+        String nome,
+
+        @NotBlank(message = "ERRO: o modelo NÃO pode ser nulo ou em BRANCO")
+        @Size(max = 60, message = "ERRO: o modelo NÃO pode conter mais do que 60 chars")
+        String modelo,
+
+        @NotBlank(message = "ERRO: a potência NÃO pode ser nula ou em BRANCO")
+        @Size(max = 30, message = "ERRO: a potência NÃO pode conter mais do que 30 chars")
+        String potencia
+
+
+) {
+}
