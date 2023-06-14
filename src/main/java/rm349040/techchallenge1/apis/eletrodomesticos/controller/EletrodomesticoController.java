c895da6c5d366b91c1ccd5e912a18a93506f7141
cesar(estudante) <estudante.pos.tech.java@gmail.com>
2023-06-14 14:39:34 -0300

    add metodo atualizar eletrodomestico ao EletrodomesticoController e cria EletrodomesticoController.java

--- src/main/java/rm349040/techchallenge1/apis/eletrodomesticos/controller/EletrodomesticoController.java
+++ src/main/java/rm349040/techchallenge1/apis/eletrodomesticos/controller/EletrodomesticoController.java
@@ -3,10 +3,10 @@ package rm349040.techchallenge1.apis.eletrodomesticos.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import rm349040.techchallenge1.apis.eletrodomesticos.controller.dtos.DadosAtualizarEletrodomestico;
 import rm349040.techchallenge1.apis.eletrodomesticos.controller.dtos.DadosCadastroEletrodomestico;
 import rm349040.techchallenge1.apis.eletrodomesticos.controller.dtos.DadosListagemEletrodomestico;
 import rm349040.techchallenge1.apis.eletrodomesticos.dominio.Eletrodomestico;
-import rm349040.techchallenge1.apis.pessoas.controller.dtos.DadosListagemPessoa;
 import rm349040.techchallenge1.repository.Repositorio;
 import rm349040.techchallenge1.util.Messages;
 import rm349040.techchallenge1.util.Validation;
@@ -32,6 +32,35 @@ public class EletrodomesticoController {
             repositorio.save(dados.toEletrodomestico());
 
             return ResponseEntity.ok().body(Messages.SUCESSO_CRIAR(Eletrodomestico.class.getSimpleName()));
+
+        } else {
+
+            return ResponseEntity.badRequest().body(validation.errorMessage(dados));
+
+        }
+
+    }
+
+
+    @PutMapping
+    public ResponseEntity atualizar(@RequestBody DadosAtualizarEletrodomestico dados) {
+
+        if (validation.isDadosOk(dados)) {
+
+            var eletrodomestico = repositorio.getReferenceById(dados.id());
+
+
+            if (eletrodomestico.isPresent()) {
+
+                eletrodomestico.get().atualizarDados(dados.toEletrodomestico());
+                repositorio.save(eletrodomestico.get());
+
+                return ResponseEntity.ok().body(dados);
+
+            } else {
+                return ResponseEntity.badRequest().body(Messages.ERRO_ATUALIZAR(Eletrodomestico.class.getSimpleName(),dados.id()));
+            }
+
 
         } else {
 
---
+++ src/main/java/rm349040/techchallenge1/apis/eletrodomesticos/controller/dtos/DadosAtualizarEletrodomestico.java
@@ -0,0 +1,35 @@
+package rm349040.techchallenge1.apis.eletrodomesticos.controller.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import rm349040.techchallenge1.apis.eletrodomesticos.dominio.Eletrodomestico;
+
+public record DadosAtualizarEletrodomestico(
+
+
+        @NotNull(message = "ERRO: o ID NÃO pode ser nulo")
+        Long id,
+
+        @NotBlank(message = "ERRO: o nome NÃO pode ser nulo ou em BRANCO")
+        @Size(max = 60, message = "ERRO: o nome do eletrodoméstico NÃO pode conter mais do que 60 chars")
+        String nome,
+
+        @NotBlank(message = "ERRO: o modelo NÃO pode ser nulo ou em BRANCO")
+        @Size(max = 60, message = "ERRO: o modelo NÃO pode conter mais do que 60 chars")
+        String modelo,
+
+        @NotBlank(message = "ERRO: a potência NÃO pode ser nula ou em BRANCO")
+        @Size(max = 30, message = "ERRO: a potência NÃO pode conter mais do que 30 chars")
+        String potencia
+
+
+) {
+
+
+    public Eletrodomestico toEletrodomestico() {
+        return new Eletrodomestico(null,nome,modelo,potencia);
+    }
+
+
+}
