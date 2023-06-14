35a9519e8bc8b5d4eb823d44a3cbb51f87c0b94c
cesar(estudante) <estudante.pos.tech.java@gmail.com>
2023-06-14 18:14:52 -0300

    add @ExceptionHandler GLOBAL ao AppConfiguration.java, para retornar mensagem de Erro, quando chegar alguma request com mensagem não legível.

--- src/main/java/rm349040/techchallenge1/config/AppConfiguration.java
+++ src/main/java/rm349040/techchallenge1/config/AppConfiguration.java
@@ -1,14 +1,19 @@
 package rm349040.techchallenge1.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import rm349040.techchallenge1.apis.enderecos.dominio.Endereco;
 import rm349040.techchallenge1.util.Messages;
 
 @Configuration
@@ -25,5 +30,11 @@ public class AppConfiguration {
     public @ResponseBody ResponseEntity handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException ex){
         return ResponseEntity.badRequest().body(Messages.ERRO_METODO_REQUEST_NAO_SUPORTADO());
     }
+
+    @ExceptionHandler(value = {HttpMessageNotReadableException.class, MethodArgumentTypeMismatchException.class})
+    public @ResponseBody ResponseEntity handleHttpMessageNotReadableException(HttpServletRequest request, Exception ex){
+        return  ResponseEntity.badRequest().body(Messages.ERRO_MENSAGEM_NAO_LEGIVEL(request,ex));
+    }
+
 
 }
--- src/main/java/rm349040/techchallenge1/util/Messages.java
+++ src/main/java/rm349040/techchallenge1/util/Messages.java
@@ -1,5 +1,7 @@
 package rm349040.techchallenge1.util;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 public class Messages {
 
     static public String SUCESSO_CRIAR(String dominio) {
@@ -19,21 +21,43 @@ public class Messages {
         return dominio + " NÃO atualizado, pois seu id=" + id + " NÃO existia na base de dados.";
     }
 
+
+    public static String ERRO_ATUALIZAR() {
+        return "Recurso NÃO atualizado, pois seu id NÃO existia na base de dados.";
+    }
+
     static public String ERRO_EXCLUIR(String dominio, long id) {
         return dominio + " NÃO excluido, pois seu id=" + id + " NÃO existia na base de dados.";
     }
 
+    public static String ERRO_EXCLUIR() {
+        return "Recurso NÃO excluido, pois seu id NÃO existia na base de dados.";
+    }
+
 
     static public String ERRO_ID_NULO(String dominio) {
-        return "ERRO: O id do "+dominio+" NÃO pode ser nulo.";
+        return "ERRO: O id do " + dominio + " NÃO pode ser nulo.";
     }
 
     static public String ERRO_CRIAR(String dominio, String msg) {
-        return "ERRO: "+dominio+""+msg;
+        return "ERRO: " + dominio + "" + msg;
     }
 
 
     static public String ERRO_METODO_REQUEST_NAO_SUPORTADO() {
         return "ERRO: Método Http NÃO suportado";
+    }
+
+    public static String ERRO_MENSAGEM_NAO_LEGIVEL(HttpServletRequest request, Exception ex) {
+        String action="";
+
+        switch (request.getMethod().toString()){
+            case "GET"   :action="LISTAR";break;
+            case "POST"  :action="CRIAR";break;
+            case "PUT"   :action="ATUALIZAR";break;
+            case "DELETE":action="EXCLUIR";break;
+        }
+
+        return "ERRO ao "+action+ " : "+ex.getMessage();
     }
 }
