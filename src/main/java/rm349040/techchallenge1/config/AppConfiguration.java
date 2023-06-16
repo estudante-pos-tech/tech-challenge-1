cbf3146604f278b9c602d8a92af1ccad3c830808
cesar(estudante) <estudante.pos.tech.java@gmail.com>
2023-06-16 13:15:56 -0300

    remove imports inuteis da AppConfiguration.java

--- src/main/java/rm349040/techchallenge1/config/AppConfiguration.java
+++ src/main/java/rm349040/techchallenge1/config/AppConfiguration.java
@@ -6,12 +6,9 @@ import jakarta.validation.Validator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import rm349040.techchallenge1.util.Messages;
 
 @Configuration
