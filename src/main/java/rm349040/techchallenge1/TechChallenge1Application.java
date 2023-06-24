c77162e345d8dc2792308e45725cee8d3b7b0d35
cesar(estudante) <estudante.pos.tech.java@gmail.com>
2023-06-24 16:39:22 -0300

    add xml support

--- src/main/java/rm349040/techchallenge1/TechChallenge1Application.java
+++ src/main/java/rm349040/techchallenge1/TechChallenge1Application.java
@@ -2,12 +2,21 @@ package rm349040.techchallenge1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class TechChallenge1Application {
+public class TechChallenge1Application implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(TechChallenge1Application.class, args);
     }
 
+
+//    @Override
+//    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+//        //configurer.defaultContentType(MediaType.APPLICATION_XML);
+//        WebMvcConfigurer.super.configureContentNegotiation(configurer);
+//    }
 }
