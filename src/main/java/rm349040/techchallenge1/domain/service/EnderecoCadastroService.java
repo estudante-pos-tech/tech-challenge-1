31a716970e54d4c6fd2b672d75217be6c045a8f2
cesar(estudante) <estudante.pos.tech.java@gmail.com>
2023-06-27 07:56:02 -0300

    remove método main

--- src/main/java/rm349040/techchallenge1/domain/service/EnderecoCadastroService.java
+++ src/main/java/rm349040/techchallenge1/domain/service/EnderecoCadastroService.java
@@ -2,7 +2,6 @@ package rm349040.techchallenge1.domain.service;
 
 import rm349040.techchallenge1.domain.exception.DomainException;
 import rm349040.techchallenge1.domain.exception.EnderecoNotFoundException;
-import rm349040.techchallenge1.domain.exception.IdNullException;
 import rm349040.techchallenge1.domain.model.Endereco;
 
 public class EnderecoCadastroService extends CadastroService<Endereco> {
@@ -16,16 +15,4 @@ public class EnderecoCadastroService extends CadastroService<Endereco> {
         return new EnderecoNotFoundException(String.format(ENTITY_NOT_FOUND_MSG(),id));
     }
 
-    public static void main(String[] args) {
-        EnderecoCadastroService service = new EnderecoCadastroService();
-
-        try{
-
-            throw service.entityNotFoundException(10l);
-
-        }catch (EnderecoNotFoundException e){
-            System.out.println(e.getMessage());
-        }
-
-    }
 }
