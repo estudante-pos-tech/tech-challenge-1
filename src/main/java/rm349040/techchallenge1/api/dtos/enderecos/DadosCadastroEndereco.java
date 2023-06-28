41123445f130ac8c67052a866718611d2780b2ef
cesar(estudante) <estudante.pos.tech.java@gmail.com>
2023-06-28 17:10:34 -0300

    ajustes no controller e no GlobalExceptionHandler.java e no  messages.properties para customizar mensagens de violaçoes de constraints

--- src/main/java/rm349040/techchallenge1/api/dtos/enderecos/DadosCadastroEndereco.java
+++ src/main/java/rm349040/techchallenge1/api/dtos/enderecos/DadosCadastroEndereco.java
@@ -3,22 +3,23 @@ package rm349040.techchallenge1.api.dtos.enderecos;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+
 public record DadosCadastroEndereco(
 
-        @NotBlank(message = "não pode ser nula ou em BRANCO")
-        @Size(max = 60, message = "nãopode conter mais do que 60 chars")
+        @NotBlank
+        @Size(max = 60)
         String rua,
-        @NotBlank(message = "não pode ser nulo ou em BRANCO")
-        @Size(max = 10, message = "não pode conter mais do que 10 chars")
+        @NotBlank()
+        @Size(max = 10)
         String numero,
-        @NotBlank(message = "não pode ser nulo ou em BRANCO")
-        @Size(max = 40, message = "não pode conter mais do que 40 chars")
+        @NotBlank
+        @Size(max = 40)
         String bairro,
-        @NotBlank(message = "não pode ser nula ou em BRANCO")
-        @Size(max = 50, message = "não pode conter mais do que 50 chars")
+        @NotBlank
+        @Size(max = 50)
         String cidade,
-        @NotBlank(message = "não pode ser nulo ou em BRANCO")
-        @Size(max = 30, message = "não pode conter mais do que 30 chars")
+        @NotBlank
+        @Size(max = 30)
         String estado
 
 
--- src/main/java/rm349040/techchallenge1/api/exceptionhandler/GlobalExceptionHandler.java
+++ src/main/java/rm349040/techchallenge1/api/exceptionhandler/GlobalExceptionHandler.java
@@ -6,6 +6,9 @@ import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.fasterxml.jackson.databind.exc.PropertyBindingException;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.TypeMismatchException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -16,17 +19,21 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.LocaleContextResolver;
 import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import rm349040.techchallenge1.domain.exception.EntityNotFoundException;
 
 import java.time.Instant;
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
+    @Autowired
+    private MessageSource messageSource;
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
@@ -38,10 +45,15 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         BindingResult bindingResult = ex.getBindingResult();
 
         List<ApiError.Field> fields = bindingResult.getFieldErrors().stream()
-                .map(fieldError -> ApiError.Field.builder()
+                .map(fieldError -> {
+
+                    String message = messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
+
+                    return ApiError.Field.builder()
                         .name(fieldError.getField())
-                        .userMessage(fieldError.getDefaultMessage())
-                        .build())
+                        .userMessage(message)
+                        .build();})
+
                 .collect(Collectors.toList());
 
         ApiError error = newApiBuilder(HttpStatus.valueOf(status.value()), errorType, detail)
