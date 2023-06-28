b0586f1e8900b718d3067ac107b6b43858835cf6
cesar(estudante) <estudante.pos.tech.java@gmail.com>
2023-06-28 20:28:19 -0300

    Tentativa migrar para messages.properties as mensagens de erros de validacoes.

--- src/main/java/rm349040/techchallenge1/api/dtos/eltrodomesticos/DadosAtualizarEletrodomestico.java
+++ src/main/java/rm349040/techchallenge1/api/dtos/eltrodomesticos/DadosAtualizarEletrodomestico.java
@@ -10,16 +10,16 @@ public record DadosAtualizarEletrodomestico(
         @NotNull(message = "ERRO: o ID NÃO pode ser nulo")
         Long id,
 
-        @NotBlank(message = "ERRO: o nome NÃO pode ser nulo ou em BRANCO")
-        @Size(max = 60, message = "ERRO: o nome do eletrodoméstico NÃO pode conter mais do que 60 chars")
+        @NotBlank(message = "O nome NÃO pode ser nulo ou em BRANCO")
+        @Size(max = 60, message = "O nome do eletrodoméstico NÃO pode conter mais do que 60 chars")
         String nome,
 
-        @NotBlank(message = "ERRO: o modelo NÃO pode ser nulo ou em BRANCO")
-        @Size(max = 60, message = "ERRO: o modelo NÃO pode conter mais do que 60 chars")
+        @NotBlank(message = "O modelo NÃO pode ser nulo ou em BRANCO")
+        @Size(max = 60, message = "O modelo NÃO pode conter mais do que 60 chars")
         String modelo,
 
-        @NotBlank(message = "ERRO: a potência NÃO pode ser nula ou em BRANCO")
-        @Size(max = 30, message = "ERRO: a potência NÃO pode conter mais do que 30 chars")
+        @NotBlank(message = "A potência NÃO pode ser nula ou em BRANCO")
+        @Size(max = 30, message = "A potência NÃO pode conter mais do que 30 chars")
         String potencia
 
 
--- src/main/java/rm349040/techchallenge1/api/dtos/eltrodomesticos/DadosCadastroEletrodomestico.java
+++ src/main/java/rm349040/techchallenge1/api/dtos/eltrodomesticos/DadosCadastroEletrodomestico.java
@@ -6,16 +6,16 @@ import jakarta.validation.constraints.Size;
 public record DadosCadastroEletrodomestico(
 
 
-        @NotBlank(message = "ERRO: o nome NÃO pode ser nulo ou em BRANCO")
-        @Size(max = 60, message = "ERRO: o nome do eletrodoméstico NÃO pode conter mais do que 60 chars")
+        @NotBlank(message = "O nome NÃO pode ser nulo ou em BRANCO")
+        @Size(max = 60, message = "O nome do eletrodoméstico NÃO pode conter mais do que 60 chars")
         String nome,
 
-        @NotBlank(message = "ERRO: o modelo NÃO pode ser nulo ou em BRANCO")
-        @Size(max = 60, message = "ERRO: o modelo NÃO pode conter mais do que 60 chars")
+        @NotBlank(message = "O modelo NÃO pode ser nulo ou em BRANCO")
+        @Size(max = 60, message = "O modelo NÃO pode conter mais do que 60 chars")
         String modelo,
 
-        @NotBlank(message = "ERRO: a potência NÃO pode ser nula ou em BRANCO")
-        @Size(max = 30, message = "ERRO: a potência NÃO pode conter mais do que 30 chars")
+        @NotBlank(message = "A potência NÃO pode ser nula ou em BRANCO")
+        @Size(max = 30, message = "A potência NÃO pode conter mais do que 30 chars")
         String potencia
 
 
