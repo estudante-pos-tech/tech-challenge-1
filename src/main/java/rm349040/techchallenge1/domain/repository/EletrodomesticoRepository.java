9cebda556e597283237b5b484ab4ec345513c84a
cesar(estudante) <estudante.pos.tech.java@gmail.com>
2023-06-29 07:56:12 -0300

    add temporary() metodo para criar uma instancia usada nos testes ingênuos da api

--- src/main/java/rm349040/techchallenge1/domain/repository/EletrodomesticoRepository.java
+++ src/main/java/rm349040/techchallenge1/domain/repository/EletrodomesticoRepository.java
@@ -1,9 +1,23 @@
 package rm349040.techchallenge1.domain.repository;
 
 import rm349040.techchallenge1.domain.model.Eletrodomestico;
+import rm349040.techchallenge1.domain.model.Pessoa;
+import rm349040.techchallenge1.domain.model.Sexo;
+
+import java.time.LocalDate;
 
 public class EletrodomesticoRepository extends Repositorio<Eletrodomestico>{
     public EletrodomesticoRepository() {
         super(Eletrodomestico.class);
+        //temporary();
+    }
+    private void temporary() {
+        Eletrodomestico temporary = Eletrodomestico.builder()
+                .id(2848467342194617996L)
+                .nome("")
+                .modelo("")
+                .potencia("")
+                .build();
+        collection.add(temporary);
     }
 }
