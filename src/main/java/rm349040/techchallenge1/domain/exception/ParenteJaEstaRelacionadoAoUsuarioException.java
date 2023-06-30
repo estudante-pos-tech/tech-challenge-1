10f2af66429b9fde2baec411e7389865e4f70089
cesar(estudante) <estudante.pos.tech.java@gmail.com>
2023-06-30 14:22:47 -0300

    implements cria classe Usuario.java e as Exceptions ParenteJaEstaRelacionadoAoUsuarioException.java e UsuarioJaEstaRelacionadoAPessoaException.java add métodos à classe Pessoa.java para dar suporte aos relacionamentos entre PESSOA e USUARIO

---
+++ src/main/java/rm349040/techchallenge1/domain/exception/ParenteJaEstaRelacionadoAoUsuarioException.java
@@ -0,0 +1,7 @@
+package rm349040.techchallenge1.domain.exception;
+
+public class ParenteJaEstaRelacionadoAoUsuarioException extends DomainException {
+    public ParenteJaEstaRelacionadoAoUsuarioException(String msg) {
+        super(msg);
+    }
+}
--- src/main/java/rm349040/techchallenge1/domain/model/Pessoa.java
+++ src/main/java/rm349040/techchallenge1/domain/model/Pessoa.java
@@ -2,8 +2,15 @@ package rm349040.techchallenge1.domain.model;
 
 import lombok.*;
 import rm349040.techchallenge1.domain.BASE;
+import rm349040.techchallenge1.domain.exception.IdNullException;
+import rm349040.techchallenge1.domain.exception.ParenteJaEstaRelacionadoAoUsuarioException;
+import rm349040.techchallenge1.domain.exception.UsuarioJaEstaRelacionadoAPessoaException;
 
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -20,6 +27,47 @@ public class Pessoa extends BASE<Pessoa> {
     private Sexo sexo;
     private Parentesco parentesco;
 
+    private Collection<Usuario> meusUsuarios;
+
+
+    public void relacionarUsuario(Usuario usuario){
+
+        if(usuario.getId() == null) throw new IdNullException("Erro ao relacionar: Id do(a) usuário não pode ser null.");
+
+
+        if(meusUsuarios == null){
+            meusUsuarios = new HashSet<>();
+        }
+
+        if( ! meusUsuarios.add(usuario) ){
+
+            String exMsg="Erro ao relacionar: usuario id="+usuario.getId()
+                    + " já está relacionada(o) a esta pessoa id="+this.getId();
+
+            throw new UsuarioJaEstaRelacionadoAPessoaException(exMsg);
+        }
+
+    }
+
+    public void desrelacionarUsuario(Usuario usuario) {
+
+        if (usuario.getId() == null) throw new IdNullException("Erro ao desrelacionar usuário da pessoa " +
+                (this.getId() != null ? "id= " + this.getId().toString() : "") + " : id do usuário não pode ser null.");
+
+        if(meusUsuarios == null){
+            meusUsuarios = new HashSet<>();
+        }
+
+        if ( ! meusUsuarios.remove(usuario) ) {
+
+            String exMsg = "Erro ao desrelacionar: o usuário id=" + usuario.getId() + " NÃO está relacionado à pessoa "
+                    + (this.getId() != null ? this.getId().toString() : "");
+
+            throw new UsuarioJaEstaRelacionadoAPessoaException(exMsg);
+        }
+    }
+
+
     /**
      * @param pessoa
      */
@@ -29,6 +77,24 @@ public class Pessoa extends BASE<Pessoa> {
         this.nascimento = pessoa.getNascimento();
         this.sexo = pessoa.getSexo();
         this.parentesco = pessoa.getParentesco();
+    }
+
+
+    public static void main(String[] args) {
+
+        Usuario u = new Usuario();
+        u.setId(999l);
+
+        Usuario u2 = new Usuario();
+        u2.setId(888l);
+
+        Pessoa p = Pessoa.builder().id(1111l).parentesco(Parentesco.AVÓ).build();
+
+        p.relacionarUsuario(u);
+        p.relacionarUsuario(u2);
+
+        p.desrelacionarUsuario(u2);
+        p.desrelacionarUsuario(u2);
     }
 
  }
